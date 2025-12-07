c5a2b689af793b5d45f9cae5039762a2d6d2c29e
W_H <hju013961@gmail.com>
2025-12-08 02:22:35 +0900

    feat: 스킬 UI 요소 추가

--- GameProgramming_Final/Assets/Scripts/MainUI/ShopStateController.cs
+++ GameProgramming_Final/Assets/Scripts/MainUI/ShopStateController.cs
@@ -1,9 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class ShopStateController : BaseStateController
 {
+    [Header("Skill UI References")]
+    [SerializeField] private Button[] skillButtons;
+    [SerializeField] private TMP_Text skillTitleText;
+    [SerializeField] private TMP_Text skillDescText;
+    [SerializeField] private TMP_Text skillCostText;
+    [SerializeField] private TMP_Text skillLevelText;
+    [SerializeField] private TMP_Text currencyText;
+    [SerializeField] private Button buyButton;
+
     protected override void Reset()
     {
         base.Reset();
